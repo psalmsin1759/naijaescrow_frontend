b1e8c9ba4898da77f93d471414c98ae32941706e
Psalmsin1759 <samson_ude@yahoo.com>
2025-10-22 11:38:13 +0100

    feat(epic6): showcase section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@ import AboutSection from "@/features/about/components/AboutSection";
 import HeroSection from "@/features/hero/components/HeroSection";
 import ServicesSection from "@/features/services/components/ServicesSection";
 import ShowcaseSection from "@/features/showcase/components/ShowcaseSection";
-import ServicesSection from "@/features/services/components/ServicesSection";
 
 
 export default function Home() {
@@ -10,6 +9,8 @@ export default function Home() {
    <div>
     <HeroSection /> 
     <AboutSection />
+    <ServicesSection />
+    <ShowcaseSection />
    </div>
   );
 }
